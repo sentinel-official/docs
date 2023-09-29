9fce8fb2b32a1c287ff04945329c0ea2a6d477e3
CommunityStaking <43610279+CommunityStaking@users.noreply.github.com>
2023-09-29 13:20:51 +0200

    Header update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -123,7 +123,7 @@ const config = {
       navbar: {
         // NOTE: hideOnScroll breaks on `/api`, enable when fixed
         // hideOnScroll: true,
-        title: 'SENTINEL',
+        title: 'SENTINEL DOCS',
         logo: {
           href: 'https://sentinel.co',
           src: '/logo/sentinel.svg',
