b8a1a9d0435b4d4347f236eed72e880c4a3d1138
CommunityStaking <43610279+CommunityStaking@users.noreply.github.com>
2023-09-29 13:20:20 +0200

    Header update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const SECTIONS = [
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Sentinel',
+  title: 'Sentinel Docs',
   tagline: 'Build on the leading decentralized VPN. 🚀',
   // TODO: Update base url
   url: 'https://docs.sentinel.co',
