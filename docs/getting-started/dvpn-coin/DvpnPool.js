06ac4ea8db8fa3b8dbe3b7f4b1a4f064b4f6dfbd
trinityprivacy <trimorneo@proton.me>
2024-07-09 16:40:16 +1000

    docs(fixed economics and telegram section): fixed economics and telegram section
    fixed economics and telegram section

--- docs/getting-started/dvpn-coin/DvpnPool.js
+++ docs/getting-started/dvpn-coin/DvpnPool.js
@@ -1,94 +1,143 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 
+const API_URLS = {
+  pool: 'https://lcd.sentinel.co/cosmos/staking/v1beta1/pool',
+  inflation: 'https://lcd.sentinel.co/cosmos/mint/v1beta1/inflation',
+  supply: 'https://api.supply.sentinel.co/',
+  vested: 'https://api.supply.sentinel.co/',
+  circulating: 'https://api.supply.sentinel.co/',
+  community: 'https://api.supply.sentinel.co/',
+};
+
 const DvpnPool = () => {
-  const [poolData, setPoolData] = useState(null);
-  const [inflationData, setInflationData] = useState(null);
-  const [supplyData, setSupplyData] = useState(null);
-  const [circulatingSupply, setCirculatingSupply] = useState(null);
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const poolUrl = 'https://lcd.sentinel.co/cosmos/staking/v1beta1/pool';
-  const inflationUrl = 'https://lcd.sentinel.co/cosmos/mint/v1beta1/inflation';
-  const supplyUrl = 'https://lcd.sentinel.co/cosmos/bank/v1beta1/supply/udvpn';
-
-  const fetchData = async (url, setDataFunction) => {
+  const fetchData = async () => {
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setDataFunction(data);
-    } catch (error) {
-      console.error(`Error fetching data from ${url}:`, error);
+      const responses = await Promise.all(
+        Object.values(API_URLS).map(url => fetch(url))
+      );
+
+      const jsonData = await Promise.all(responses.map(res => res.json()));
+
+      const [poolData, inflationData, supplyData, vestedData, circulatingData, communityData] = jsonData;
+
+      setData({
+        poolData,
+        inflationData,
+        supplyData,
+        vestedData,
+        circulatingData,
+        communityData,
+      });
+      setLoading(false);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+      setError('Failed to fetch data');
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const formatNumber = (number) => {
-    if (number === null) {
-      return 'N/A';
-    }
-
+    if (number === null) return 'N/A';
+    
+    // Check if number is in billions range
     const billion = 1e9;
+    const million = 1e6;
+  
     if (number >= billion) {
       return (number / billion).toFixed(2) + 'B';
-    }
-    return number.toFixed(2);
-  };
-
-  const calculateCirculatingSupply = () => {
-    if (supplyData && poolData && inflationData) {
-      const totalSupply = parseFloat(supplyData.amount.amount) / 1e6; // Convert to DVPN
-      const bondedTokens = parseFloat(poolData.pool.bonded_tokens) / 1e6; // Convert to DVPN
-      const circulatingSupply = totalSupply - bondedTokens;
-      setCirculatingSupply(circulatingSupply);
+    } else if (number >= million) {
+      return (number / million).toFixed(2) + 'M';
+    } else {
+      return number.toFixed(2);
     }
   };
+  
 
-  useEffect(() => {
-    fetchData(poolUrl, setPoolData);
-    fetchData(inflationUrl, setInflationData);
-    fetchData(supplyUrl, setSupplyData);
-  }, [poolUrl, inflationUrl, supplyUrl]);
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Fetching Data...</h1>;
 
-  useEffect(() => {
-    calculateCirculatingSupply();
-  }, [supplyData, poolData, inflationData]);
+  const {
+    poolData,
+    inflationData,
+    supplyData,
+    vestedData,
+    circulatingData,
+    communityData,
+  } = data;
+
+  const totalSupply = parseFloat(supplyData.total) / 1e6;
+  const circulating = parseFloat(circulatingData.circulation) / 1e6;
+  const vested = parseFloat(vestedData.vesting) / 1e6;
+  const community = parseFloat(communityData.community_pool) / 1e6;
 
   return (
-    <>
-      {poolData && inflationData && supplyData ? (
-        <table>
-          <thead>
-            <tr>
-              <th>
-                <code>Attribute</code>
-              </th>
-              <th>
-                <code>Value</code>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Circulating Tokens</td>
-              <td>{formatNumber(circulatingSupply)}</td>
-            </tr>
-            <tr>
-              <td>Bonded Tokens</td>
-              <td>{formatNumber(parseFloat(poolData.pool.bonded_tokens) / 1e6)}</td> {/* Convert to DVPN */}
-            </tr>
-            <tr>
-              <td>Total Supply</td>
-              <td>{formatNumber(parseFloat(supplyData.amount.amount) / 1e6)}</td> {/* Convert to DVPN */}
-            </tr>
-            <tr>
-              <td>Inflation</td>
-              <td>{inflationData.inflation !== null ? (parseFloat(inflationData.inflation) * 100).toFixed(1) + '%' : 'N/A'}</td>
-            </tr>
-          </tbody>
-        </table>
-      ) : (
-        <h1>Fetching Data...</h1>
-      )}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <code>
+              <b>Attribute</b>
+            </code>
+          </th>
+          <th>
+            <code>
+              <b>Value</b>
+            </code>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bonded</td>
+          <td>{formatNumber(parseFloat(poolData.pool.bonded_tokens) / 1e6)}</td>
+        </tr>
+        <tr>
+          <td>Inflation</td>
+          <td>
+            {inflationData.inflation !== null
+              ? (parseFloat(inflationData.inflation) * 100).toFixed(1) + '%'
+              : 'N/A'}
+          </td>
+        </tr>
+        <tr>
+          <td>Supply</td>
+          <td>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <b>Total</b>
+                  </td>
+                  <td>
+                    <b>{formatNumber(totalSupply)}</b>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Circulating</td>
+                  <td>{formatNumber(circulating)}</td>
+                </tr>
+                <tr>
+                  <td>Vested</td>
+                  <td>{formatNumber(vested)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>Community Pool</td>
+          <td>{formatNumber(community)}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
--- docs/getting-started/telegram/official.md
+++ docs/getting-started/telegram/official.md
@@ -8,32 +8,33 @@ sidebar_position: 1
 
 ## Official
 
-- [Sentinel dVPN](https://t.me/sentineldvpn): The official main community channel
-- [Sentinel Official Announcements](https://t.me/Sentinel_Announcements): Official Sentinel news and announcements
-- [Sentinel Node Network](https://t.me/SentinelNodeNetwork): A group for Sentinel node operators
+- [Sentinel dVPN](https://t.me/sentineldvpn): The official main community channel.
+- [Sentinel Official Announcements](https://t.me/Sentinel_Announcements): Official Sentinel news and announcements.
+- [Sentinel Node Network](https://t.me/SentinelNodeNetwork): A group for dVPN node operators.
+- [Sentinel RPC Node Network](https://t.me/sentinelrpcnodes): A group for RPC Node operators.
 
 ## Community
 
-- [Sentinel Growth DAO](https://t.me/SentinelGrowthDAO): A place for everyone to discuss and participate in the Sentinel Growth DAO
-- [Sentinel DVPN Price Chat](https://t.me/dvpnpricediscussion): Price chat and DVPN coin discussion
-- [Sentinel Coin Governance Group](https://t.me/SentinelGov): Governance and validator discussion
+- [Sentinel Growth DAO](https://t.me/SentinelGrowthDAO): A place for everyone to discuss and participate in the Sentinel Growth DAO.
+- [Sentinel DVPN Price Chat](https://t.me/dvpnpricediscussion): Price chat and DVPN coin discussion.
+- [Sentinel Coin Governance Group](https://t.me/SentinelGov): Governance and validator discussion.
 - [Sentinel dVPN Application Testing](https://t.me/VPNproducttesting): A group where user can report their app testing outcomes.
-- [Sentinel Independent News (S.I.N.)](https://t.me/sentinel_independent): Unofficial Sentinel news channel
-- [The S.I.N. Bin](https://t.me/the_sinbin): Submissions, tips, comments, feedback, etc. for S.I.N.
-- [DVPN Degens (Trollbox 3.0)](https://t.me/+EEXWvqA4hho2ZDMx): Anything goes
-- [Sentinel Bureau of Memetic Warfare](https://t.me/Sentinelmemewar): A channel which posts OC Sentinel memes
+- [Sentinel Independent News (S.I.N.)](https://t.me/sentinel_independent): Unofficial Sentinel news channel.
+- [The S.I.N. Bin](https://t.me/the_sinbin): Submissions, tips, comments, feedback, etc. for Sentinel Independent News.
+- [Sentinel Free Speech](https://t.me/Sentinelfreespeech): The Legendary Sentinel Free Speech Group. All are welcome. No malicous images or files allowed.
+- [Sentinel Bureau of Memetic Warfare](https://t.me/Sentinelmemewar): A channel which posts OC Sentinel memes.
 
 ## Whitelables
 
-- [Solar dVPN](https://t.me/solarlabs):  Announcement channel, but there is also a [Community Group](https://t.me/solarlabs_chat),
-- [Meile dVPN](https://t.me/MathNodes): Everything regarding Meile dVPN is discussed in this very active group
-- [Independent dVPN](https://t.me/independentdvpn): The Turkish free dVPN that is becoming very popular
-- [V2:App](https://t.me/v2app): A dVPN App powered by the advanced V2Ray technology
-- [Dogwifhat dVPN](https://t.me/dogwifhatdvpn): The Sentinel Ecosystem's first foray into the memecoin world.
+- [Solar dVPN](https://t.me/solarlabs):  Announcement channel, but there is also a [Community Group](https://t.me/solarlabs_chat).
+- [Meile dVPN](https://t.me/MathNodes): Everything regarding Meile dVPN is discussed in this very active group.
+- [Independent dVPN](https://t.me/independentdvpn): The Turkish free dVPN that is becoming very popular.
+- [V2:App](https://t.me/v2app): A dVPN App powered by the advanced V2Ray technology.
+- [Dogwifhat dVPN](https://t.me/dogwifhatdvpn): The Sentinel Ecosystem's first foray into the memecoin world..
 
 
 ## Utility Bots
 
-- [Sentinel Validators Missed Blocks](https://t.me/sentinel_missed): Automated channel for monitoring validator missed blocks and jailings/unjailings
-- [Sentinel Whale Monitor](https://t.me/sentinel_whale_monitor): Automated channel for monitoring big transactions and foundation-related wallets
-- [Sentinel Node Monitor](https://t.me/dvpn_node_bot): A free Telegram bot designed for Sentinel node operators to monitor node information and status
+- [Sentinel Validators Missed Blocks](https://t.me/sentinel_missed): Automated channel for monitoring validator missed blocks and jailings/unjailings.
+- [Sentinel Whale Monitor](https://t.me/sentinel_whale_monitor): Automated channel for monitoring big transactions and foundation-related wallets.
+- [Sentinel Node Monitor](https://t.me/dvpn_node_bot): A free Telegram bot designed for Sentinel node operators to monitor node information and status.
