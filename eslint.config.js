2ad6d688a9b2d38a05c579372b7ef94f752182b7
trimorneo <trimorneo@proton.me>
2025-04-28 17:44:30 +1000

    docs(updated eslint.config.js for ignoring build/): updated eslint.config.js for ignoring build/
    updated eslint.config.js for ignoring build/

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,33 @@
-module.exports = {
-    settings: {
-      react: {
-        version: "detect"
-      }
-    },
-    rules: {
-      "react/prop-types": "off",
-      "no-unused-vars": ["warn", { "varsIgnorePattern": "^React$" }] // Ignore 'React' in unused vars check
-    },
+module.exports = [
+  {
     ignores: [
       "build/",
       "node_modules/",
       "*.cjs",
       "sidebars-*.js"
     ],
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     languageOptions: {
       parserOptions: {
         ecmaFeatures: {
-          jsx: true
+          jsx: true,
         },
         ecmaVersion: 2021,
-        sourceType: "module"
+        sourceType: "module",
       },
       globals: {
-        // Define your global variables here
-        // For example:
-        // "window": true,
-        // "document": true
-      }
-    }
-  };  
+        // You can define global variables here
+      },
+    },
+    rules: {
+      "react/prop-types": "off",
+      "no-unused-vars": ["warn", { "varsIgnorePattern": "^React$" }],
+    },
+  },
+];
